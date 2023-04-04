727739b3a6a8c0d07200bb015ce1c8af939f78a4
puritymwaura <purity.mwaura@student.moringaschool.com>
2023-04-04 11:02:07 +0300

    final changes

--- src/component/form.js
+++ src/component/form.js
@@ -1,101 +1,228 @@
-// import React, { useState } from "react";
-// export default function Form() {
-//   const [validationErrors, setValidationErrors] = useState({
-//     name: "",
-//     email: "",
-//     password: "",
-//     phonenumber: "",
-//     arcade: "",
-//     advanced: "",
-//     pro: "",
-//     online: "",
-//     largerstorage: "",
-//     customizeprofile: "",
-//     arcademonthly: "",
-//     onlineservice: "",
-//     totalCost: "",
-//   });
-//   if (page === 0) {
-//     if (formData.name.length < 3) {
-//       setValidationErrors({
-//         ...validationErrors,
-//         name: "Length should be greater than 3",
-//       });
-//       setErrors(true);
-//     } else {
-//       setValidationErrors({
-//         ...validationErrors,
-//         name: "",
-//       });
-//       setErrors(false);
-//     }
-//   }
-//   return (
-//     <button
-//       onClick={() => {
-//         if (page === FormTitles.length - 1) {
-//           alert("FORM SUBMITTED");
-//           console.log(formData);
-//         } else {
-//           let hasErrors = false;
-//           if (page === 0) {
-//             if (formData.name.length < 3) {
-//               setValidationErrors({
-//                 ...validationErrors,
-//                 name: "Length should be greater than 3",
-//               });
-//               hasErrors = true;
-//             } else {
-//               setValidationErrors({
-//                 ...validationErrors,
-//                 name: "",
-//               });
-//             }
-//           }
+// // import React, { useState } from "react";
+// // export default function Form() {
+// //   const [validationErrors, setValidationErrors] = useState({
+// //     name: "",
+// //     email: "",
+// //     password: "",
+// //     phonenumber: "",
+// //     arcade: "",
+// //     advanced: "",
+// //     pro: "",
+// //     online: "",
+// //     largerstorage: "",
+// //     customizeprofile: "",
+// //     arcademonthly: "",
+// //     onlineservice: "",
+// //     totalCost: "",
+// //   });
+// //   if (page === 0) {
+// //     if (formData.name.length < 3) {
+// //       setValidationErrors({
+// //         ...validationErrors,
+// //         name: "Length should be greater than 3",
+// //       });
+// //       setErrors(true);
+// //     } else {
+// //       setValidationErrors({
+// //         ...validationErrors,
+// //         name: "",
+// //       });
+// //       setErrors(false);
+// //     }
+// //   }
+// //   return (
+// //     <button
+// //       onClick={() => {
+// //         if (page === FormTitles.length - 1) {
+// //           alert("FORM SUBMITTED");
+// //           console.log(formData);
+// //         } else {
+// //           let hasErrors = false;
+// //           if (page === 0) {
+// //             if (formData.name.length < 3) {
+// //               setValidationErrors({
+// //                 ...validationErrors,
+// //                 name: "Length should be greater than 3",
+// //               });
+// //               hasErrors = true;
+// //             } else {
+// //               setValidationErrors({
+// //                 ...validationErrors,
+// //                 name: "",
+// //               });
+// //             }
+// //           }
 
-//           console.log(validationErrors);
-//           if (!hasErrors) {
-//             setPage((page) => page + 1);
-//             setErrors(false);
-//           }
-//         }
-//       }}
-//     >
-//       {page === FormTitles.length - 1 ? "Submit" : "Next"}
-//     </button>
+// //           console.log(validationErrors);
+// //           if (!hasErrors) {
+// //             setPage((page) => page + 1);
+// //             setErrors(false);
+// //           }
+// //         }
+// //       }}
+// //     >
+// //       {page === FormTitles.length - 1 ? "Submit" : "Next"}
+// //     </button>
+// //   );
+// // }largerstorage: "",
+
+// // // ...
+
+// // // ...
+
+// import React, { useState } from "react";
+// import PersonalInfo from "./personalInfo";
+// import SelectYourPlan from "./selectYourPlan";
+// import PickAddOns from "./pickAddOns";
+// import FinishUp from "./finishingUp";
+// import { getname } from "../store/appState";
+
+// function Form() {
+//   const [page, setPage] = useState(0);
+//   const [formData,setFormData] = useState({})
+// //   const [formData, setFormData] = useState({
+// //     name: "",
+// //     email: "",
+// //     password: "",
+// //     phonenumber: "",
+// //     arcade: "",
+// //     advanced: "",
+// //     pro: "",
+// //     online: "",
+// //     largerstorage: "",
+// //     customizeprofile: "",
+// //     arcademonthly: "",
+// //     onlineservice: "",
+// //     total: "",
+// //   });
+
+//   const [errors, setErrors] = useState([]);
+// //   const [validationErrors, setValidationErrors] = useState({
+// //     name: "",
+// //     email: "",
+// //     password: "",
+// //     phonenumber: "",
+// //     arcade: "",
+// //     advanced: "",
+// //     pro: "",
+// //     online: "",
+// //     largerstorage: "",
+// //     customizeprofile: "",
+// //     arcademonthly: "",
+// //     onlineservice: "",
+// //     total: "",
+// //   });
+
+//   const FormTitles = ["Personal Info", "Select Plan", "Pick Add Ons", "Finish"];
+
+//   const PageDisplay = () => {
+//     if (page === 0) {
+//       return (
+//         <PersonalInfo
+//           formData={formData}
+//           name={getname}
+//           setFormData={setFormData}
+//             errors={errors}
+//         />
+//       );
+//     } else if (page === 1) {
+//       return <SelectYourPlan formData={formData} setFormData={setFormData} />;
+//     } else if (page === 2) {
+//       return <PickAddOns formData={formData} setFormData={setFormData} />;
+//     } else {
+//       return <FinishUp formData={formData} setFormData={setFormData} />;
+//     }
+//   };
+//   console.log(formData)
+
+//   return (
+//     <div className="form">
+//       <div className="progressbar">
+//         <div
+//           style={{
+//             width:
+//               page === 0
+//                 ? "25%"
+//                 : page === 1
+//                 ? "25%"
+//                 : page === 1
+//                 ? "25%"
+//                 : "25%",
+//           }}
+//         ></div>
+//       </div>
+//       <div className="form-container">
+//         <div className="header">
+//           <h1>{FormTitles[page]}</h1>
+//         </div>
+//         <div className="body">{PageDisplay()}</div>
+//         <div className="footer">
+//           <button
+//             disabled={page === 0}
+//             onClick={() => {
+//               setPage((page) => page - 1);
+//             }}
+//           >
+//             Prev
+//           </button>
+//           <button
+//             onClick={() => {
+//               if (page === FormTitles.length - 1) {
+//                 alert("FORM SUBMITTED");
+//                 console.log(formData);
+//               } else {
+//                 // show errors
+//                 if (page === 0) {
+//                   if (formData.name && formData.name.length < 3) { // added null check
+//                     setErrors([...errors, "Too few characters in the name"]);
+//                   }
+//                 }
+                
+//                 // if (page === 0) {
+//                 //   if (formData.name.length < 3) {
+//                 //     setErrors([
+//                 //       ...errors,
+//                 //       "Too few characters in the name",
+//                 //     ]);
+//                 //     // setErrors(true);
+//                 //   }
+//                 // }
+
+//                 // console.log(validationErrors);
+//                 if (errors === false) {
+//                   setPage((page) => page + 1);
+//                 }
+
+//                 //
+//               }
+//             }}
+//           >
+//             {page === FormTitles.length - 1 ? "Submit" : "Next"}
+//           </button>
+//           {/* {errors.length > 0 && (
+//                                 <ul style={{color:"red"}}className="fs-5">
+//                                     {errors.map(error => <li key={error}>{error}</li>)}
+//                                 </ul>
+//                             )} */}
+//         </div>
+//       </div>
+//     </div>
 //   );
 // }
 
-// // ...
+// export default Form;
 
-// // ...
+
+// 
+
+
+// ...
+
+// ...
 
 import React, { useState } from "react";
-import PersonalInfo from "./personalInfo";
-import SelectYourPlan from "./selectYourPlan";
-import PickAddOns from "./pickAddOns";
-import FinishUp from "./finishingUp";
-import { getname } from "../store/appState";
 
-function Form() {
-  const [page, setPage] = useState(0);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    phonenumber: "",
-    arcade: "",
-    advanced: "",
-    pro: "",
-    online: "",
-    largerstorage: "",
-    customizeprofile: "",
-    arcademonthly: "",
-    onlineservice: "",
-    total: "",
-  });
-
-  const [errors, setErrors] = useState(false);
+export default function Form() {
   const [validationErrors, setValidationErrors] = useState({
     name: "",
     email: "",
@@ -109,92 +236,63 @@ function Form() {
     customizeprofile: "",
     arcademonthly: "",
     onlineservice: "",
-    total: "",
+    totalCost: "",
   });
 
-  const FormTitles = ["Personal Info", "Select Plan", "Pick Add Ons", "Finish"];
+  // Define the missing variables
+  const [page, setPage] = useState(0);
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState(false);
+  const FormTitles = ["Personal Info", "Select Your Plan", "Pick Add-Ons", "Finish Up"];
 
-  const PageDisplay = () => {
-    if (page === 0) {
-      return (
-        <PersonalInfo
-          formData={formData}
-          name={getname}
-          setFormData={setFormData}
-          validationErrors={validationErrors}
-        />
-      );
-    } else if (page === 1) {
-      return <SelectYourPlan formData={formData} setFormData={setFormData} />;
-    } else if (page === 2) {
-      return <PickAddOns formData={formData} setFormData={setFormData} />;
+  if (page === 0) {
+    if (formData.name.length < 3) {
+      setValidationErrors({
+        ...validationErrors,
+        name: "name should be greater than 3",
+      });
+      setErrors(true);
     } else {
-      return <FinishUp formData={formData} setFormData={setFormData} />;
+      setValidationErrors({
+        ...validationErrors,
+        name: "",
+      });
+      setErrors(false);
     }
-  };
+  }
 
   return (
-    <div className="form">
-      <div className="progressbar">
-        <div
-          style={{
-            width:
-              page === 0
-                ? "25%"
-                : page === 1
-                ? "25%"
-                : page === 1
-                ? "25%"
-                : "25%",
-          }}
-        ></div>
-      </div>
-      <div className="form-container">
-        <div className="header">
-          <h1>{FormTitles[page]}</h1>
-        </div>
-        <div className="body">{PageDisplay()}</div>
-        <div className="footer">
-          <button
-            disabled={page === 0}
-            onClick={() => {
-              setPage((page) => page - 1);
-            }}
-          >
-            Prev
-          </button>
-          <button
-            onClick={() => {
-              if (page === FormTitles.length - 1) {
-                alert("FORM SUBMITTED");
-                console.log(formData);
-              } else {
-                // show errors
-                if (page === 0) {
-                  if (formData.name.length < 3) {
-                    setValidationErrors({
-                      ...validationErrors,
-                      name: "Length should be greater than 3",
-                    });
-                    setErrors(true);
-                  }
-                }
+    <button
+      onClick={() => {
+        if (page === FormTitles.length - 1) {
+          alert("FORM SUBMITTED");
+          console.log(formData);
+        } else {
+          let hasErrors = false;
+          if (page === 0) {
+            if (formData.name.length < 3) {
+              setValidationErrors({
+                ...validationErrors,
+                name: "name should be greater than 3",
+              });
+              hasErrors = true;
+            } else {
+              setValidationErrors({
+                ...validationErrors,
+                name: "",
+              });
+            }
+          }
 
-                console.log(validationErrors);
-                if (errors === false) {
-                  setPage((page) => page + 1);
-                }
-
-                //
-              }
-            }}
-          >
-            {page === FormTitles.length - 1 ? "Submit" : "Next"}
-          </button>
-        </div>
-      </div>
-    </div>
+          console.log(validationErrors);
+          if (!hasErrors) {
+            setPage((page) => page + 1);
+            setErrors(false);
+          }
+        }
+      }}
+    >
+      {page === FormTitles.length - 1 ? "Submit" : "Next"}
+    </button>
   );
 }
-
-export default Form;
--- src/component/personalInfo.js
+++ src/component/personalInfo.js
@@ -1,3 +1,45 @@
+// import { useState } from 'react';
+// import { useNavigate } from "react-router-dom";
+// function PersonalInfo({ formData, setFormData }) {
+//   const [errors, setErrors] = useState({ name: '', email: '', phoneNumber: '' });
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData({ ...formData, [name]: value });
+
+//     // validation logic for each input field
+//     switch (name) {
+//       case 'name':
+//         setErrors({ ...errors, name: value.length < 3 ? 'Name must be at least 3 characters long' : '' });
+//         break;
+//       case 'email':
+//         setErrors({ ...errors, email: !/\S+@\S+\.\S+/.test(value) ? 'Invalid email address' : '' });
+//         break;
+//       case 'phoneNumber':
+//         setErrors({ ...errors, phoneNumber: !/^\d{10}$/.test(value) ? 'Phone number must be 10 digits long' : '' });
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   return (
+//     <div className="personal-info-container">
+//       {/* display error messages alongside the input fields */}
+//       {errors.name && <span className="error-message">{errors.name}</span>}
+//       <input type="text" placeholder=" Name..." name="name" value={formData.name} onChange={handleInputChange} />
+
+//       {errors.email && <span className="error-message">{errors.email}</span>}
+//       <input type="text" placeholder="email..." name="email" value={formData.email} onChange={handleInputChange} />
+
+//       {errors.phoneNumber && <span className="error-message">{errors.phoneNumber}</span>}
+//       <input type="text" placeholder="Phonenumber..." name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} />
+//     </div>
+//   );
+// }
+
+// export default PersonalInfo;
+
 import React from "react";
 
 function PersonalInfo({ formData, setFormData, validationErrors }) {
